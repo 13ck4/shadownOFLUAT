ab1df6131361620ec1f04d388c9e5ff5effb265b
luat <nguyen-ngoc-luat@datanext.co.jp>
2021-08-18 17:47:38 +0700

    update

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Get perfect shadow every time for the non-designer.
 
 # Installation 
 
-`npm i shadowizard --save`
+`npm i shadowOFLUAT --save`
 
 Then... 
 
 ```
-import { shadowizard } form 'shadowizard';
-shadowizard({
+import { shadowOFLUAT } form 'shadowOFLUAT';
+shadowOFLUAT({
     shadow_type: 'soft',
     padding: false
 });
@@ -21,7 +21,7 @@ shadowizard({
 
 ## Options
 
-Shadowizard supports 2 options, both of which are optional:
+shadowOFLUAT supports 2 options, both of which are optional:
 
 * *shadow_type* -_hard | soft_ (Defaults to soft)
 * *padding* -_boolean_ (Defaults to false)
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function shadowizard(options){
-    let images = document.querySelectorAll('.shadowizard');
+function shadowOFLUAT(options){
+    let images = document.querySelectorAll('.shadowOFLUAT');
 
     if(options.shadow_type === 'hard')
         options.shadow_type = '0px';
@@ -7,10 +7,10 @@ function shadowizard(options){
         options.shadow_type = '15px';
 
     images.forEach(image => {
-        image.style.bosShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+        image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
         if(options.padding){
             image.style.padding = '1em';
         }
     })
 }
-module.exports.shadowluat = shadowizard;
+module.exports.shadowOFLUAT = shadowOFLUAT;
